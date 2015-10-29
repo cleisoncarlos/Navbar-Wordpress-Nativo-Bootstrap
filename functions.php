de36fe074339b88bf31ffcddd2e897a9569892e7
Cleison Carlos <cleisonspider@yahoo.com.br>
2015-10-29 16:19:41 -0300

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,3 @@
+<!--  MENU  ----------------------------------------------- -->
+	<?php require_once(bloginfo('template_directory').'/wp_bootstrap_navwalker.php'); ?>
+		<?php register_nav_menus( array('primary' => __( 'Primary Menu', 'mentor' ), ) ); ?>
