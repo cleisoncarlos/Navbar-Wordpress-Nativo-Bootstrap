abb682d15a305e842c281a870e85414e686287e9
Jacob <jacoblisboa@gmail.com>
2015-11-04 13:03:16 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,3 +1,6 @@
 <!--  MENU  ----------------------------------------------- -->
-	<?php require_once(bloginfo('template_directory').'/wp_bootstrap_navwalker.php'); ?>
+	<?php
+	include (TEMPLATEPATH .'/wp_bootstrap_navwalker.php'); 
+	
+	?>
 		<?php register_nav_menus( array('primary' => __( 'Primary Menu', 'mentor' ), ) ); ?>
